b96863b77dd5171477e5a530677fddfaec40ee1c
Eddy Moran <doyourhomework85@gmail.com>
2023-04-16 17:01:20 +0000

    mini-project-week12

--- List Of AWS Services.py
+++ List Of AWS Services.py
@@ -1,1 +1,31 @@
-print("List Of AWS Services")
+#Mini-Project
+
+#Create a list of AWS services
+print("Mini-Project - List Of AWS Services")
+
+#The list should be empty at first
+services_list = []
+print("Empty list")
+
+#Populate the list using append function, print the list
+services_list.append("DynamoDB, RDS, VPC, S3, EC2, CodeDeploy, IAM, Cloudwatch")
+print(services_list)
+
+#Create a filled list
+services_list = ['DynamoDB', 'RDS', 'VPC', 'S3', 'EC2', 'CodeDeploy', 'IAM', 'CloudWatch']
+print(services_list)
+
+#Print the length of the list
+print("List Length: ",len(services_list))
+
+#Remove two specific AWS services
+#item 1 - list printed
+services_list.remove('RDS')
+print(services_list)
+
+#Item 2 - list printed
+services_list.remove('S3')
+print(services_list)
+
+#Print updated length of the list
+print("List Length: ",len(services_list))
